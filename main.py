6ef41a1e2662ce3809d40a930e01c9e4c12ee0a5
minu <alsdn638@gmail.com>
2025-12-19 20:57:50 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,251 @@
+import math
+import pygame
+import sys
+import time
+from minos import *
+
+pygame.init()
+SCREEN_W = 1600
+SCREEN_H = 900
+screen = pygame.display.set_mode((SCREEN_W, SCREEN_H))
+
+clock = pygame.time.Clock()
+
+UNIT = 32
+
+class RNG:
+    def __init__(self, seed):
+        self.t = seed % 2147483647
+
+        if self.t == 0:
+            self.t += 2147483646
+
+    def next(self):
+        self.t = 16807 * self.t % 2147483647
+        return self.t
+    
+    def nextFloat(self):
+        return (self.next() - 1) / 2147483646
+
+    def shuffleArray(self, array):
+        if len(array) == 0:
+            return array
+
+        for i in range(len(array)-1, 0, -1):
+            r = math.floor(self.nextFloat() * (i + 1))
+            array[i], array[r] = array[r], array[i]
+
+        return array
+
+class Board:
+    def __init__(self, w, h, margin_top):
+        self.w = w
+        self.h = h
+        self.margin_top = margin_top
+        self.grid = [[" "]*w for _ in range(h)]
+
+    def draw(self, screen, pos):
+        for y in range(self.h):
+            for x in range(self.w):
+                rect = (x*UNIT+pos[0], y*UNIT+pos[1], UNIT, UNIT)
+                if self.grid[y][x] == " ":
+                    if y >= self.margin_top:
+                        pygame.draw.rect(screen, MINO_COLORS["X"], rect, 1)
+                else:
+                    pygame.draw.rect(screen, MINO_COLORS[self.grid[y][x]], rect)
+
+    def line_clear(self):
+        for y in range(self.h-1, -1, -1):
+            for x in range(self.w):
+                if self.grid[y][x] == " ":
+                    break
+            else:
+                self.grid.pop(y)
+                self.grid.insert(0, [" "]*self.w)
+
+    def place(self, mino):
+        for y, row in enumerate(MINO_SHAPES[mino.type][str(mino.rotation)]):
+            for x, dot in enumerate(row):
+                if dot:
+                    self.grid[mino.y+y][mino.x+x] = mino.type
+        self.line_clear()
+
+class Handler:
+    def __init__(self, das, arr, sdf):
+        self.das = das
+        self.arr = arr
+        self.sdf = sdf
+
+        self.held = []
+
+        self.right_hold_ms = 0
+        self.right_arr_timer = 0
+
+        self.left_hold_ms = 0
+        self.left_arr_timer = 0
+
+        self.soft_drop_held = False
+        self.soft_drop_sdf_timer = 0
+
+    def down_right(self):
+        self.held.append("right")
+        self.right_hold_ms = 0
+        self.right_arr_timer = 0
+
+    def down_left(self):
+        self.held.append("left")
+        self.left_hold_ms = 0
+        self.left_arr_timer = 0
+
+    def down_soft_drop(self):
+        self.soft_drop_held = True
+        self.soft_drop_sdf_timer = 0
+
+    def up_right(self):
+        self.held.remove("right")
+
+    def up_left(self):
+        self.held.remove("left")
+
+    def up_soft_drop(self):
+        self.soft_drop_held = False
+
+    def update(self, dt, board):
+        movement_queue = []
+
+        if self.soft_drop_held:
+            self.soft_drop_sdf_timer += dt
+            for _ in range(board.h):
+                if self.soft_drop_sdf_timer >= self.sdf:
+                    self.soft_drop_sdf_timer -= self.sdf
+                    movement_queue.append((0, 1))
+                else:
+                    break
+
+        if self.held == []:
+            return movement_queue
+
+        if self.held[-1] == "right":
+            self.right_hold_ms += dt
+            if self.right_hold_ms >= self.das:
+                self.right_arr_timer += dt
+                for _ in range(board.w):
+                    if self.right_arr_timer >= self.arr:
+                        self.right_arr_timer -= self.arr
+                        movement_queue.append((1, 0))
+                    else:
+                        break
+                    
+        if self.held[-1] == "left":
+            self.left_hold_ms += dt
+            if self.left_hold_ms >= self.das:
+                self.left_arr_timer += dt
+                for _ in range(board.w):
+                    if self.left_arr_timer >= self.arr:
+                        self.left_arr_timer -= self.arr
+                        movement_queue.append((-1, 0))
+                    else:
+                        break
+
+        return movement_queue
+
+class Game:
+    def __init__(self):
+        self.board = Board(10, 40, 20)
+        self.rng = RNG(time.time()*1000)
+        self.queue = []
+        self.next()
+        self.handler = Handler(117, 0, 0)
+
+    def draw_mino(self, screen, pos):
+        for y, row in enumerate(MINO_SHAPES[self.mino.type][str(self.mino.rotation)]):
+            for x, dot in enumerate(row):
+                if dot:
+                    rect = ((self.mino.x+x)*UNIT+pos[0], (self.mino.y+y)*UNIT+pos[1], UNIT, UNIT)
+                    pygame.draw.rect(screen, MINO_COLORS[self.mino.type], rect)
+    
+    def next(self):
+        if len(self.queue) <= 5:
+            self.queue += self.rng.shuffleArray(MINO_TYPES.copy())
+        self.mino = self.pop_queue()
+
+    def hard_drop(self):
+        for _ in range(self.board.h):
+            if self.mino.move(0, 1, self.board) == False:
+                self.board.place(self.mino)
+                self.next()
+                break
+
+    def keydown(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.handler.down_right()
+            self.mino.move(1, 0, self.board)
+        if event.key == pygame.K_LEFT:
+            self.handler.down_left()
+            self.mino.move(-1, 0, self.board)
+        if event.key == pygame.K_DOWN:
+            self.handler.down_soft_drop()
+        if event.key == pygame.K_UP:
+            self.mino.rotate(1, self.board)
+        if event.key == pygame.K_LCTRL:
+            self.mino.rotate(-1, self.board)
+        if event.key == pygame.K_a:
+            self.mino.rotate(2, self.board)
+        if event.key == pygame.K_SPACE:
+            self.hard_drop()
+
+    def keyup(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.handler.up_right()
+        if event.key == pygame.K_LEFT:
+            self.handler.up_left()
+        if event.key == pygame.K_DOWN:
+            self.handler.up_soft_drop()
+
+    def update(self, dt):
+        movement_queue = self.handler.update(dt, self.board)
+        for x, y in movement_queue:
+            self.mino.move(x, y, self.board)
+
+    def draw_shadow(self, screen, pos):
+        shadow_mino = Mino(self.mino.type, self.mino.x, self.mino.y, self.mino.rotation)
+        for _ in range(self.board.h):
+            if shadow_mino.move(0, 1, self.board) == False:
+                break
+        for y, row in enumerate(MINO_SHAPES[self.mino.type][str(self.mino.rotation)]):
+            for x, dot in enumerate(row):
+                if dot:
+                    rect = ((shadow_mino.x+x)*UNIT+pos[0], (shadow_mino.y+y)*UNIT+pos[1], UNIT, UNIT)
+                    pygame.draw.rect(screen, MINO_COLORS["H"], rect)
+
+    def draw(self, screen):
+        pos = (SCREEN_W/2-UNIT*10/2, SCREEN_H/2-UNIT*20/2)
+        margin_pos = (pos[0], pos[1]-self.board.margin_top*UNIT)
+        self.board.draw(screen, margin_pos)
+        self.draw_shadow(screen, margin_pos)
+        self.draw_mino(screen, margin_pos)
+
+    def fill_queue(self):
+        self.queue += self.rng.shuffleArray(MINO_TYPES.copy())
+
+    def pop_queue(self):
+        return Mino(self.queue.pop(0), 3, self.board.margin_top-4)
+
+game = Game()
+
+while True:
+    screen.fill("#333333")
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            game.keydown(event)
+        if event.type == pygame.KEYUP:
+            game.keyup(event)
+
+    game.draw(screen)
+    dt = clock.tick(120)
+    game.update(dt)
+
+    pygame.display.update()
